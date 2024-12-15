158122a1fd27867836002bfc620512cc47a54396
Юлия <coroleva.yu@gmail.com>
2024-12-15 14:26:41 +0300

    add file and readme
    Добавили файлы общий, меню, майн, редми

---
+++ menu.py
@@ -0,0 +1,7 @@
+# область переменных
+menu_mode = True
+menu_options = ['Возврат в игру', 'Новая игра', 'Сохранить', 'Загрузить', 'Выход']
+menu_current_index = 3
+menu_options_id = []
+
+pause = False
